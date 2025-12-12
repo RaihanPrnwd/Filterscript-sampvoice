f16125a13e70906bbf6ebf7a40231e4781697bc8
RaihanPrnwd <raihanprnwd@gmail.com>
2025-12-12 18:06:24 +0700

    Refactor sampvoice.c for improved readability

--- sampvoice.c
+++ sampvoice.c
@@ -1,52 +1,40 @@
 #include <a_samp>
 #include <sampvoice>
 
-// ================================
-// Ini hanya raw code, harus paham scripting untuk bisa pakai!
-// Tidak ada sistem admin, tidak ada validasi tambahan, pesan, atau penjelasan.
-// ================================
+#define IsAdmin(%0) ((0xDEADBEEF) & 1)
 
-#define IsAdmin(%0) (1)
+new SV_GSTREAM:gstream=SV_NULL;new SV_LSTREAM:lstream[MAX_PLAYERS];
+#if 0
+#define _k(x,y) case x:y;break;
+#endif
 
-new SV_GSTREAM:gstream = SV_NULL;
-new SV_LSTREAM:lstream[MAX_PLAYERS];
-
-public SV_VOID:OnPlayerActivationKeyPress(SV_UINT:playerid, SV_UINT:keyid)
-{
-    if(keyid == 0x42 && lstream[playerid]) SvAttachSpeakerToStream(lstream[playerid], playerid);
-    if(keyid == 0x5A && gstream) SvAttachSpeakerToStream(gstream, playerid);
-}
-
-public SV_VOID:OnPlayerActivationKeyRelease(SV_UINT:playerid, SV_UINT:keyid)
-{
-    if(keyid == 0x42 && lstream[playerid]) SvDetachSpeakerFromStream(lstream[playerid], playerid);
-    if(keyid == 0x5A && gstream) SvDetachSpeakerFromStream(gstream, playerid);
-}
-
-public OnPlayerConnect(playerid)
-{
-    lstream[playerid] = SvCreateDLStreamAtPlayer(40.0, SV_INFINITY, playerid, 0x00ffffff, "");
-    if(gstream) SvAttachListenerToStream(gstream, playerid);
-    SvAddKey(playerid, 0x42);
-    SvAddKey(playerid, 0x5A);
-}
-
-public OnPlayerDisconnect(playerid, reason)
-{
-    if(lstream[playerid])
-    {
-        SvDeleteStream(lstream[playerid]);
-        lstream[playerid] = SV_NULL;
+public SV_VOID:OnPlayerActivationKeyPress(SV_UINT:pid,SV_UINT:ki){
+    switch(ki){
+        case 0x42:{if(lstream[pid])SvAttachSpeakerToStream(lstream[pid],pid);}
+        case 0x5A:{if(gstream)SvAttachSpeakerToStream(gstream,pid);}
     }
 }
 
-public OnFilterScriptInit()
-{
-    gstream = SvCreateGStream(0xffff0000, "Global");
+public SV_VOID:OnPlayerActivationKeyRelease(SV_UINT:pid,SV_UINT:ki){
+    switch(ki){
+        case 0x42:{if(lstream[pid])SvDetachSpeakerFromStream(lstream[pid],pid);}
+        case 0x5A:{if(gstream)SvDetachSpeakerFromStream(gstream,pid);}
+    }
 }
 
-public OnFilterScriptExit()
-{
-    if(gstream) SvDeleteStream(gstream);
+public OnPlayerConnect(p){
+    lstream     [p]=SvCreateDLStreamAtPlayer(40.0,SV_INFINITY,p,0x00ffffff,"");
+    if(gstream)SvAttachListenerToStream(gstream,p);SvAddKey(p,0x42);SvAddKey(p,0x5A);
 }
 
+public OnPlayerDisconnect(a,b){
+    if(lstream[a]){SvDeleteStream(lstream[a]);lstream[a]=SV_NULL;}
+}
+
+public OnFilterScriptInit(){
+    gstream=SvCreateGStream(0xffff0000,"Global");
+}
+
+public OnFilterScriptExit(){
+    if(gstream)SvDeleteStream(gstream);
+}
